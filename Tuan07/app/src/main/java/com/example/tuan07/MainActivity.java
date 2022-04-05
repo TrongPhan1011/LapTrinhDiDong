5e3c48a6e7311bc2ecca58cc794e2eb9dc69bd13
TrongPhan1011 <huutrong2k1@gmail.com>
2022-04-05 18:16:47 +0700

    Tuan07 add,cancel,load

---
+++ Tuan07/app/src/main/java/com/example/tuan07/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.tuan07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    SqliteHelper db;
+    List<Item> ls;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = new SqliteHelper(this);
+
+
+
+
+        ls = new ArrayList<Item>();
+        ls = db.getAllItem();
+
+        ListView listView = findViewById(R.id.lvItem);
+
+        ItemAdapter adapter = new ItemAdapter(this, ls, R.layout.item,db);
+        listView.setAdapter(adapter);
+
+        handleAdd();
+
+        handleCancel();
+
+
+    }
+
+    public  void showList(){
+        ls = db.getAllItem();
+
+        ListView listView = findViewById(R.id.lvItem);
+
+        ItemAdapter adapter = new ItemAdapter(this, ls, R.layout.item,db);
+        listView.setAdapter(adapter);
+
+        handleAdd();
+    }
+
+
+    public void handleAdd(){
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText txt = findViewById(R.id.txt);
+                String text = txt.getText().toString().trim();
+                if(text!= null && text.length()!=0){
+                        db.addItem(new Item(text));
+                }
+                showList();
+                txt.setText("");
+            }
+        });
+
+    }
+
+    public void handleCancel(){
+        Button btnAdd = findViewById(R.id.btnCancel);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText txt = findViewById(R.id.txt);
+                txt.setText("");
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
---
+++ Tuan07/app/src/main/java/com/example/tuan07/SqliteHelper.java
@@ -0,0 +1,92 @@
+package com.example.tuan07;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqliteHelper extends SQLiteOpenHelper {
+
+    private static final  int version = 1;
+    private static final  String databaseName = "itemDB";
+    private static final  String tableName = "item";
+    private static final  String NAME = "name";
+    private static final  String key = "id";
+
+    private Item item;
+
+
+    public SqliteHelper(@Nullable Context context) {
+        super(context, databaseName, null, version);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE " + tableName + "("
+                + key + " INTEGER PRIMARY KEY AUTOINCREMENT," + NAME + " TEXT)";
+
+        sqLiteDatabase.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + tableName);
+        onCreate(sqLiteDatabase);
+    }
+    public void addItem(Item item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAME,item.getName());
+        db.insert(tableName,null,values);
+        db.close();
+    }
+    public List<Item> getAllItem(){
+
+        List<Item> ls = new ArrayList<Item>();
+        String sql = "SELECT  * FROM " + tableName;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql,null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Item i = new Item();
+                i.setId(Integer.parseInt(cursor.getString(0)));
+                i.setName(cursor.getString(1));
+
+                ls.add(i);
+            } while (cursor.moveToNext());
+        }
+
+
+        return ls;
+    }
+
+    public int updateItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(NAME, item.getName());
+
+        return db.update(tableName, values, key + " = ?",
+                new String[] { String.valueOf(item.getId()) });
+    }
+
+    public int deleteItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        return db.delete(tableName,  key + " = ?",
+                new String[] { String.valueOf(item.getId()) });
+    }
+
+
+}
